e1de9e6a442c8e0aa1e4178de4f1bb57dcb27c6e
Rajiv <rajivsaikia22@gmail.com>
2016-07-18 12:04:50 +0530

    admin init

---
+++ includes/admin_init.php
@@ -0,0 +1,5 @@
+<?php include_once 'includes/database-config.php'; ?>
+<link href="css/profile.css" rel="stylesheet"/>
+<script src="js/profile.js"></script>
+<script src="js/list.js"></script>
+<script src="js/list.pagination.min.js"></script>
