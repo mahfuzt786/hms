87fa8d35737563c34c18d73def90d342c0d59a37
Rajiv <rajivsaikia22@gmail.com>
2016-07-18 12:08:05 +0530

    new files

---
+++ js/drugs_category.js
@@ -0,0 +1,107 @@
+$(document).ready(function(){
+    $('#drugs-category').focus();
+    $('#btn-add-category-values').on('click',function(e) {
+        e.preventDefault();
+        check();    
+    });
+    $('#btn-edit-category-values').on('click',function(e) {
+        e.preventDefault();
+        check_edit();    
+    });
+    function check_edit(){
+        if($('#edit-drugs-category').val()==""){
+            Lobibox.alert("error",
+            {
+                msg: 'Please enter Category Name!'
+            });
+        }else if($('textarea#edit-drugs-category-description').val()==""){
+            Lobibox.alert("error",
+            {
+                msg: 'Please provide Category Description !!'
+            });
+        }
+        edit_category();
+    }
+    function check(){
+        if($('#drugs-category').val()==""){
+            Lobibox.alert("error",
+            {
+                msg: 'Please enter Category Name!'
+            });
+        }else if($('textarea#drugs-category-description').val()==""){
+            Lobibox.alert("error",
+            {
+                msg: 'Please provide Category Description !!'
+            });
+        }
+        add_category();
+    }
+    function edit_category()
+    {
+        if($('#edit-drugs-category').val() && $('#edit-drugs-category-description').val())
+        {
+            $.ajax({
+                url: "includes/login-check.php",
+                type: "POST",
+                data: {
+                    action: 'editCategory',
+                    eid: $('input#catid').val(),
+                    ecat: $('input#edit-drugs-category').val(),
+                    ecat_desc: $('textarea#edit-drugs-category-description').val()
+                },
+                //dataType: "json",
+                success: function(result){
+                    if(result=='done') {
+                        Lobibox.alert("success",
+                        {
+                            msg: 'Drugs Category Successfully Updated '
+                        });
+                        
+                        if($('.lobibox'))
+                        {
+                            $('.lobibox .lobibox-btn').focus();
+                        }
+                        location.replace('drugs.php');
+                    }
+                    else {
+                        Lobibox.alert("error",
+                        {
+                            msg: result
+                        });
+                    }
+                }
+            });
+        }
+    } 
+    function add_category()
+    {
+        if($('#drugs-category').val() && $('#drugs-category-description').val())
+        {
+            $.ajax({
+                url: "includes/login-check.php",
+                type: "POST",
+                data: {
+                    action: 'addCategory',
+                    cat: $('input#drugs-category').val(),
+                    cat_desc: $('textarea#drugs-category-description').val()
+                },
+                //dataType: "json",
+                success: function(result){
+                    if(result=='done') {
+                        Lobibox.alert("success",
+                        {
+                            msg: 'Drugs Category Successfully Added '
+                        });
+                        location.replace('drugs.php');
+                    }
+                    else {
+                        Lobibox.alert("error",
+                        {
+                            msg: result
+                        });
+                    }
+                }
+            });
+        }
+    }    
+});
---
+++ js/setting.js
@@ -0,0 +1,147 @@
+$(document).ready(function(){
+    $('.check_input').hide();
+    $('#old_pass').focus();
+    $('#btn-change-password').on('click',function(e) {
+        e.preventDefault();
+        check();    
+    });
+    if($('#old_pass').val()){
+        var value = $('#old_pass').val();
+        $.ajax({
+            url: "includes/login-check.php",
+            type: "POST",
+            data: {
+                action: 'oldPassCheck',
+                id: $('input#id').val()
+            },
+            //dataType: "json",
+            success: function(result){
+                if(value==result){
+                    $('span#oldP_remove').hide();
+                    $('span#oldP_ok').show();
+                }else{
+                    $('span#oldP_ok').hide();
+                    $('span#oldP_remove').show();
+                }
+            }
+        });
+    }
+    $('#old_pass').keyup(
+        function() {
+            $('span#oldP_ok').hide();
+            $('span#oldP_remove').hide();
+            var value = document.getElementById('old_pass').value;
+            
+            $.ajax({
+                url: "includes/login-check.php",
+                type: "POST",
+                data: {
+                    action: 'oldPassCheck',
+                    id: $('input#id').val()
+                },
+                //dataType: "json",
+                success: function(result){
+                    if(value==result){
+                        $('span#oldP_remove').hide();
+                        $('span#oldP_ok').show();
+                    }else{
+                        $('span#oldP_ok').hide();
+                        $('span#oldP_remove').show();
+                    }
+                }
+            });
+        });
+    $('#new_pass').keyup(
+        function() {
+            var newpass = document.getElementById('new_pass').value;
+            var newpassc = document.getElementById('new_pass_c').value;
+            if(newpass.length < 4){
+                $('span#newP_ok').hide();
+                $('span#newP_remove').show();
+            }else{
+                $('span#newP_remove').hide();
+                $('span#newP_ok').show();
+            }
+            if(newpassc!="" && newpass!=newpassc){
+                $('span#newP_ok').hide();
+                $('span#newP_remove').show();
+            }
+        });
+    $('#new_pass_c').keyup(
+        function() {
+            var newpassc = document.getElementById('new_pass_c').value;
+            var newpass = document.getElementById('new_pass').value;
+            if(newpass==newpassc){
+                $('span#newPc_remove').hide();
+                $('span#newPc_ok').show();
+            }else{
+                $('span#newPc_ok').hide();
+                $('span#newPc_remove').show();
+            }
+        });
+    function check(){
+        $new_p = $('#new_pass').val();
+        $new_p_c = $('#new_pass_c').val();
+        if($('#old_pass').val()==""){
+            Lobibox.alert("error",
+            {
+                msg: 'Please enter Password !'
+            });
+        }else if($('#new_pass').val()==""){
+            Lobibox.alert("error",
+            {
+                msg: 'Please enter New Password !!'
+            });
+        }else if($new_p.length < 4){
+            Lobibox.alert("error",
+            {
+                msg: 'Password must not be less than 3 character !'
+            });
+        }else if($('#new_pass_c').val()==""){
+            Lobibox.alert("error",
+            {
+                msg: 'Please Re-Type New Password !!!'
+            });
+        }else if($new_p != $new_p_c){
+            Lobibox.alert("error",
+            {
+                msg: 'New Password Mismatch !!!'
+            });
+        }else{
+            change_password();
+        }
+    }
+    function change_password()
+    {
+        if($('#old_pass').val() && $('#new_pass').val() && $('#new_pass_c').val())
+        {
+            $.ajax({
+                url: "includes/login-check.php",
+                type: "POST",
+                data: {
+                    action: 'changePassword',
+                    id: $('input#id').val(),
+                    username: $('input#username').val(),
+                    oldPass: $('input#old_pass').val(),
+                    newPass: $('input#new_pass').val()
+                },
+                //dataType: "json",
+                success: function(result){
+                    if(result=='done') {
+                        Lobibox.alert("success",
+                        {
+                            msg: 'Password successfully updated !!'
+                        });
+                        location.replace('includes/logout.php');
+                    }
+                    else {
+                        Lobibox.alert("error",
+                        {
+                            msg: result
+                        });
+                    }
+                }
+            });
+        }
+    }    
+});
