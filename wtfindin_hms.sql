b3a7c85263f9219c4fc3582abc0f1b273a10695a
Rajiv <rajivsaikia22@gmail.com>
2016-07-22 17:54:25 +0530

    new database table

--- wtfindin_hms.sql
+++ wtfindin_hms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 21, 2016 at 03:00 PM
+-- Generation Time: Jul 22, 2016 at 02:22 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -19,6 +19,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `wtfindin_hms`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE IF NOT EXISTS `doctor` (
+`d_id` int(100) NOT NULL,
+  `d_name` varchar(100) NOT NULL,
+  `d_designation` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`d_id`, `d_name`, `d_designation`) VALUES
+(1, 'Syed Mahfuzul Mazid', 'MD Gynecologist'),
+(2, 'Rajiv Saikia', 'MD Medicine');
 
 -- --------------------------------------------------------
 
@@ -45,13 +65,11 @@ CREATE TABLE IF NOT EXISTS `drugs` (
 --
 
 INSERT INTO `drugs` (`drugs_id`, `drugs_cat_id`, `drugs_name`, `drugs_description`, `drugs_price`, `drugs_company`, `drugs_quantity`, `drugs_manufacturing_date`, `drugs_expiry_date`, `drugs_entry_date`, `drugs_status`) VALUES
-(1, 1, 'Some Vitamin', 'Vitamin C', 250, 'WalknetFunctions', 100, '12/3/13', '18/9/18', '2016-07-18 17:57:47', 'a'),
 (2, 4, 'Nicip Plus', 'Paracetamol Tablet for Fever', 10, 'ITC', 300, '20/1/12', '16/2/16', '2016-07-19 15:21:37', 'e'),
-(3, 0, 'nice', 'nice tablets for fever and cold', 12, 'r and d company ltd.', 14, '13/04/2018', '100', '2016-07-21 09:42:27', 'a'),
-(4, 7, 'livsav plus', 'liver tonic', 130, 'Gayetri company', 15, '24/10/2018', '200', '2016-07-21 09:48:38', 'a'),
+(3, 4, 'nice', 'nice tablets for fever and cold', 10, 'r and d company ltd.', 90, '13/04/2018', '24/02/2017', '2016-07-21 18:30:49', 'a'),
+(4, 7, 'livsav plus', 'liver tonic', 130, 'Gayetri company', 15, '24/10/2018', '30/12/2016', '2016-07-21 18:31:04', 'a'),
 (5, 8, 'citricoto', 'alergy lotion', 140, 'citric acid ltd', 100, '07/09/2015', '20/07/2017', '2016-07-21 09:52:37', 'a'),
-(6, 6, 'blueberry', 'antacid for accidity', 140, 'ss company', 100, '16/06/2015', '21/09/2017', '2016-07-21 10:03:09', 'a'),
-(7, 4, 'sss', 'sssss', 0, 'sss', 12, '06/07/2016', '27/07/2016', '2016-07-21 10:03:55', 'a');
+(7, 4, 'sss', 'sssss', 90, 'sss', 100, '06/07/2016', '27/07/2016', '2016-07-22 06:54:58', 'a');
 
 -- --------------------------------------------------------
 
@@ -71,12 +89,81 @@ CREATE TABLE IF NOT EXISTS `drugscategory` (
 --
 
 INSERT INTO `drugscategory` (`drugs_cat_id`, `drugs_cat`, `drugs_cat_desc`, `drugs_cat_date`) VALUES
-(1, 'Vitamin Tabs', 'Vitamin Tablets', '2016-07-17 17:26:46'),
 (4, 'Paracetamol', 'Paracetamol tablets and syprup', '2016-07-17 19:41:16'),
 (5, 'Pain Killers', 'Pain Killers tablets and injection\n', '2016-07-17 20:07:45'),
-(6, 'Anta Acid', 'Anta Acidic Syprup for acidity\n', '2016-07-18 17:08:20'),
 (7, 'Liver Syrup', 'Liver Syrup and Tablets', '2016-07-18 17:10:23'),
 (8, 'Allergy', 'Allergic Tabs', '2016-07-18 17:12:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE IF NOT EXISTS `employee` (
+`e_id` int(100) NOT NULL,
+  `e_emp_id` varchar(100) NOT NULL,
+  `e_des_id` int(100) NOT NULL,
+  `e_name` varchar(100) NOT NULL,
+  `e_salary` int(100) NOT NULL,
+  `e_age` int(3) NOT NULL,
+  `e_gender` enum('male','female') NOT NULL,
+  `e_address` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`e_id`, `e_emp_id`, `e_des_id`, `e_name`, `e_salary`, `e_age`, `e_gender`, `e_address`) VALUES
+(1, 'EMP1001', 1, 'Debojit Deori', 10000, 27, 'male', 'Sonari Court Colony, Sibsagar'),
+(2, 'EMP1002', 2, 'Manash Jyoti Gogoi', 9000, 27, 'male', 'Meteka Gaon, Sibsagar'),
+(3, 'EMP1003', 3, 'Sridevi Karmakar', 5000, 30, 'female', 'Borsillah Tea Estate');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_designation`
+--
+
+CREATE TABLE IF NOT EXISTS `employee_designation` (
+`e_des_id` int(100) NOT NULL,
+  `e_des` varchar(30) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employee_designation`
+--
+
+INSERT INTO `employee_designation` (`e_des_id`, `e_des`) VALUES
+(1, 'Manager'),
+(2, 'Asst. Manager'),
+(3, 'Labour');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prescription`
+--
+
+CREATE TABLE IF NOT EXISTS `prescription` (
+`p_id` int(100) NOT NULL,
+  `doctor_id` int(100) NOT NULL,
+  `patient_type` enum('1','2') NOT NULL,
+  `employee_id` int(100) NOT NULL,
+  `drug_cat_id` int(100) NOT NULL,
+  `drug_id` int(100) NOT NULL,
+  `p_date` date NOT NULL,
+  `p_remark` varchar(100) NOT NULL,
+  `p_note` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `prescription`
+--
+
+INSERT INTO `prescription` (`p_id`, `doctor_id`, `patient_type`, `employee_id`, `drug_cat_id`, `drug_id`, `p_date`, `p_remark`, `p_note`) VALUES
+(1, 1, '1', 3, 4, 3, '2016-07-22', 'Fever and Cold', 'Chest X-Ray, Urine Test');
 
 -- --------------------------------------------------------
 
@@ -110,7 +197,7 @@ CREATE TABLE IF NOT EXISTS `userlogin` (
   `loginDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `userId` int(11) NOT NULL,
   `remote_addr` varchar(45) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=97 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `userlogin`
@@ -202,11 +289,27 @@ INSERT INTO `userlogin` (`userLoginId`, `loginDate`, `userId`, `remote_addr`) VA
 (83, '2016-07-20 17:54:38', 1, '127.0.0.1'),
 (84, '2016-07-20 18:06:29', 1, '127.0.0.1'),
 (85, '2016-07-21 07:10:27', 1, '127.0.0.1'),
-(86, '2016-07-21 09:41:29', 1, '127.0.0.1');
+(86, '2016-07-21 09:41:29', 1, '127.0.0.1'),
+(87, '2016-07-21 14:16:40', 1, '127.0.0.1'),
+(88, '2016-07-21 14:17:24', 1, '127.0.0.1'),
+(89, '2016-07-21 15:14:59', 1, '127.0.0.1'),
+(90, '2016-07-21 15:15:33', 1, '127.0.0.1'),
+(91, '2016-07-21 15:36:15', 1, '127.0.0.1'),
+(92, '2016-07-21 18:00:40', 1, '127.0.0.1'),
+(93, '2016-07-22 06:53:17', 1, '127.0.0.1'),
+(94, '2016-07-22 07:09:28', 1, '127.0.0.1'),
+(95, '2016-07-22 09:42:24', 1, '127.0.0.1'),
+(96, '2016-07-22 09:42:25', 1, '127.0.0.1');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+ ADD PRIMARY KEY (`d_id`);
 
 --
 -- Indexes for table `drugs`
@@ -219,6 +322,24 @@ ALTER TABLE `drugs`
 --
 ALTER TABLE `drugscategory`
  ADD PRIMARY KEY (`drugs_cat_id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+ ADD PRIMARY KEY (`e_id`);
+
+--
+-- Indexes for table `employee_designation`
+--
+ALTER TABLE `employee_designation`
+ ADD PRIMARY KEY (`e_des_id`);
+
+--
+-- Indexes for table `prescription`
+--
+ALTER TABLE `prescription`
+ ADD PRIMARY KEY (`p_id`);
 
 --
 -- Indexes for table `user`
@@ -237,6 +358,11 @@ ALTER TABLE `userlogin`
 --
 
 --
+-- AUTO_INCREMENT for table `doctor`
+--
+ALTER TABLE `doctor`
+MODIFY `d_id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT for table `drugs`
 --
 ALTER TABLE `drugs`
@@ -247,6 +373,21 @@ MODIFY `drugs_id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 ALTER TABLE `drugscategory`
 MODIFY `drugs_cat_id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+MODIFY `e_id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `employee_designation`
+--
+ALTER TABLE `employee_designation`
+MODIFY `e_des_id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `prescription`
+--
+ALTER TABLE `prescription`
+MODIFY `p_id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
@@ -255,7 +396,7 @@ MODIFY `userId` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 -- AUTO_INCREMENT for table `userlogin`
 --
 ALTER TABLE `userlogin`
-MODIFY `userLoginId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=87;
+MODIFY `userLoginId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=97;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
