b61d805480dcb5cc6151ca8b452437853bf2eb40
Rajiv <rajivsaikia22@gmail.com>
2016-08-09 18:54:02 +0530

    new file added

---
+++ js/sick-allowance-report.js
@@ -0,0 +1,222 @@
+$(document).ready(function(){
+    document.getElementById("daily-empid").disabled=true;
+    $('#d-empid').change(function(){
+        if($('#d-empid').prop('checked')){
+            document.getElementById("daily-empid").disabled=false; 
+            $('#daily-empid').focus();
+        }else{
+            document.getElementById("daily-empid").disabled=true;
+            $('#daily-date').focus();
+        }
+    });
+    $('.nav-tabs a').click(function(){
+        $(this).tab('show');
+        $('#frame').prop('src', '');
+    })
+    $("#daily-date").datepicker({
+        dateFormat:'yy-mm-dd',
+        changeMonth: true,
+        changeYear: true,
+        showOtherMonths: true,
+        selectOtherMonths: true,
+        maxDate: 0
+    });
+    /*$("#weekly-date").datepicker({
+        dateFormat:'yy-mm-dd',
+        changeMonth: true,
+        changeYear: true,
+        maxDate: 0
+    });*/
+    $("#start-date").datepicker({
+        dateFormat:'yy-mm-dd',
+        changeMonth: true,
+        changeYear: true,
+        maxDate: 0
+    });
+    $("#end-date").datepicker({
+        dateFormat:'yy-mm-dd',
+        changeMonth: true,
+        changeYear: true,
+        maxDate: 0
+    });
+    $('#btn-daily-sa').on('click',function(e) {
+        e.preventDefault();
+        dailysa();    
+    });
+    $('#btn-weekly-drug').on('click',function(e) {
+        e.preventDefault();
+        weeklydrug();    
+    });
+    
+    $('#btn-monthly-drug').on('click',function(e) {
+        e.preventDefault();
+        monthlydrug();    
+    });
+    $('#btn-yearly-drug').on('click',function(e) {
+        e.preventDefault();
+        yearlydrug();    
+    });
+    $('#btn-between-drug').on('click',function(e) {
+        e.preventDefault();
+        betweendrug();    
+    });
+    function dailysa(){
+        if($('#d-empid').prop('checked') && $('#daily-empid').val()==""){
+            Lobibox.alert("error",
+            {
+                msg: 'Please enter Employee ID!'
+            }); 
+        }else if($('#daily-date').val()==""){
+            Lobibox.alert("error",
+            {
+                msg: 'Please enter Date!'
+            });
+        }else{
+            var dat=$('input#daily-date').val();
+            $.ajax({
+                url: "service/check-drug-report.php",
+                type: "POST",
+                data: {
+                    action: 'check_daily',
+                    dt: dat
+                },
+                //dataType: "json",
+                success: function(result){
+                    if(result=='done'){
+                        $('#frame').prop('src', 'lib/tcpdf/reports/daily-drug-report.php?dat='+dat+'');
+                               
+                    }else{
+                        $('#frame').prop('src', 'lib/tcpdf/reports/no-data.php');
+                    }
+                }
+            });
+        }
+    }
+    function weeklydrug(){
+        if($('#weekly-date').val()==""){
+            Lobibox.alert("error",
+            {
+                msg: 'Please enter Date!'
+            });
+        }else{
+            var dat=$('#weekly-date').val();
+            $.ajax({
+                url: "service/check-drug-report.php",
+                type: "POST",
+                data: {
+                    action: 'check_weekly',
+                    dt: dat
+                },
+                //dataType: "json",
+                success: function(result){
+                    if(result=='done'){
+                        $('#frame').prop('src', 'lib/tcpdf/reports/weekly-drug-report.php?dat='+dat+'');
+                               
+                    }else{
+                        $('#frame').prop('src', 'lib/tcpdf/reports/no-data.php');
+                    }
+                }
+            });
+        }
+    }
+    function monthlydrug(){
+        if($('#monthly-month').val()=="select"){
+            Lobibox.alert("error",
+            {
+                msg: 'Please Select a Month!'
+            });
+        }else if($('#monthly-year').val()=="select"){
+            Lobibox.alert("error",
+            {
+                msg: 'Please Select a Year!'
+            });
+        }else{
+            var month=$('#monthly-month').val();
+            var year=$('#monthly-year').val();
+            $.ajax({
+                url: "service/check-drug-report.php",
+                type: "POST",
+                data: {
+                    action: 'check_monthly',
+                    month: month,
+                    year: year
+                },
+                //dataType: "json",
+                success: function(result){
+                    if(result=='done'){
+                        $('#frame').prop('src', 'lib/tcpdf/reports/monthly-drug-report.php?month='+month+'&year='+year+'');
+                               
+                    }else{
+                        $('#frame').prop('src', 'lib/tcpdf/reports/no-data.php');
+                    }
+                }
+            });
+        }
+    }
+    function yearlydrug(){
+        if($('#yearly-year').val()=="select"){
+            Lobibox.alert("error",
+            {
+                msg: 'Please Select a Year!'
+            });
+        }else{
+            var year=$('#yearly-year').val();
+            $.ajax({
+                url: "service/check-drug-report.php",
+                type: "POST",
+                data: {
+                    action: 'check_yearly',
+                    year: year
+                },
+                //dataType: "json",
+                success: function(result){
+                    if(result=='done'){
+                        $('#frame').prop('src', 'lib/tcpdf/reports/yearly-drug-report.php?year='+year+'');
+                               
+                    }else{
+                        $('#frame').prop('src', 'lib/tcpdf/reports/no-data.php');
+                    }
+                }
+            });
+        }
+    }
+    function betweendrug(){
+        if($('#start-date').val()==""){
+            Lobibox.alert("error",
+            {
+                msg: 'Please Select 1st Date!'
+            });
+        }else if($('#end-date').val()==""){
+            Lobibox.alert("error",
+            {
+                msg: 'Please Select 2nd Date!'
+            });
+        }else if($('#start-date').val()==$('#end-date').val()){
+            Lobibox.alert("error",
+            {
+                msg: 'Dates must not be same. Please Select different date!'
+            });
+        }else{
+            var start=$('#start-date').val();
+            var end=$('#end-date').val();
+            $.ajax({
+                url: "service/check-drug-report.php",
+                type: "POST",
+                data: {
+                    action: 'check_between',
+                    start: start,
+                    end: end
+                },
+                //dataType: "json",
+                success: function(result){
+                    if(result=='done'){
+                        $('#frame').prop('src', 'lib/tcpdf/reports/between-drug-report.php?start='+start+'&end='+end+'');
+                               
+                    }else{
+                        $('#frame').prop('src', 'lib/tcpdf/reports/no-data.php');
+                    }
+                }
+            });
+        }
+    }
+});
---
+++ sick-allowance-report.php
@@ -0,0 +1,221 @@
+
+<?php
+include 'includes/checkInvalidUser.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title >HMS : Sick Allowance Report</title>
+        <?php require_once 'includes/html_main.php'; ?>
+        <?php require_once 'includes/admin_init.php'; ?>
+        <script src="js/sick-allowance-report.js"></script>
+        <link href="css/sick-allowance-report.css" rel="stylesheet"/>
+        <link href="css/profile.css" rel="stylesheet"/>
+        <link href="css/dashboard.css" rel="stylesheet"/>
+        <script type="text/javascript">
+            $(function() {
+                var startDate;
+                var endDate;
+    
+                var selectCurrentWeek = function() {
+                    window.setTimeout(function () {
+                        $('.week-picker').find('.ui-datepicker-current-day a').addClass('ui-state-active')
+                    }, 1);
+                }
+    
+                $('.week-picker').datepicker( {
+                    showOtherMonths: true,
+                    selectOtherMonths: true,
+                    onSelect: function(dateText, inst) { 
+                        var date = $(this).datepicker('getDate');
+                        startDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay());
+                        endDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay() + 6);
+                        var dateFormat = inst.settings.dateFormat || $.datepicker._defaults.dateFormat;
+                        $('#startDate').text($.datepicker.formatDate( dateFormat, startDate, inst.settings ));
+                        $('#endDate').text($.datepicker.formatDate( dateFormat, endDate, inst.settings ));
+            
+                        selectCurrentWeek();
+                    },
+                    beforeShowDay: function(date) {
+                        var cssClass = '';
+                        if(date >= startDate && date <= endDate)
+                            cssClass = 'ui-datepicker-current-day';
+                        return [true, cssClass];
+                    },
+                    onChangeMonthYear: function(year, month, inst) {
+                        selectCurrentWeek();
+                    }
+                });
+    
+                $('.week-picker .ui-datepicker-calendar tr').live('mousemove', function() { $(this).find('td a').addClass('ui-state-hover'); });
+                $('.week-picker .ui-datepicker-calendar tr').live('mouseleave', function() { $(this).find('td a').removeClass('ui-state-hover'); });
+            });
+        </script>
+    </head>
+    <body>
+
+        <?php require_once('includes/menu_navbar.php'); ?>
+
+        <div class="toggled-2" id="wrapper"><?php
+        require_once('includes/menu_sidebar.php');
+        ?>
+            <!-- Page Content -->
+            <div id="page-content-wrapper">
+                <div class="container xyz">
+                    <div class="panel panel-default">
+                        <div class="panel-heading heading"><i class="fa fa-battery-half"> Sick-Allowance-Report</i></div>
+                        <div class="panel-body">
+                            <ul class="nav nav-tabs">
+                                <li class="active"><a data-toggle="tab" href="#daily"><i class="fa fa-battery-1"></i> Daily</a></li>
+                                <li><a data-toggle="tab" href="#between"><i class="fa fa-battery-2"></i> Between Dates</a></li>
+                                <li><a data-toggle="tab" href="#monthly"><i class="fa fa-battery-3"></i> Monthly</a></li>
+                                <li><a data-toggle="tab" href="#Yearly"><i class="fa fa-battery-4"></i> Yearly</a></li>
+                            </ul>
+
+                            <div class="tab-content">
+                                <div id="daily" class="tab-pane fade in active">
+                                    <div class="tab-rep">
+
+                                        <div class="col-md-1 drug-field">Emp. ID &nbsp;<input type="checkbox" id="d-empid" name="d-empid"></input></div>
+                                        <div class="col-md-3">
+                                            <input class="form-control" placeholder="Employee ID" type="text" id="daily-empid" name='daily-empid'/></input>
+                                        </div>
+                                        <div class="col-md-1 drug-field">Enter Date</div>
+                                        <div class="col-md-3">
+                                            <div class=" input-group">
+                                                <span class="input-group-addon" id="basic-addon1">
+                                                    <i class="fa fa-calendar"></i>
+                                                </span>
+                                                <input class="form-control" readonly type="text" id="daily-date" name='daily-date'/></input>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-1 head">
+                                            <button type="button" class="btn btn-block btn-success" name="btn-daily-sa" id="btn-daily-sa">
+                                                <span class="fa fa-server"></span> Go</button>       
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="monthly" class="tab-pane fade">
+                                    <div class="tab-rep">
+                                        <div class="col-md-1 drug-field">Month</div>
+                                        <div class="col-md-2">
+                                            <div class=" input-group">
+                                                <span class="input-group-addon" id="basic-addon1">
+                                                    <i class="fa fa-calendar"></i>
+                                                </span>
+                                                <select id="monthly-month" class="form-control">
+                                                    <option value="select">select</option>
+                                                    <?php
+                                                    $month = array("1" => "January",
+                                                        "2" => "February",
+                                                        "3" => "March",
+                                                        "4" => "April",
+                                                        "5" => "May",
+                                                        "6" => "June",
+                                                        "7" => "July",
+                                                        "8" => "August",
+                                                        "9" => "September",
+                                                        "10" => "October",
+                                                        "11" => "November",
+                                                        "12" => "December");
+                                                    foreach ($month as $k => $v) {
+                                                        echo '<option value=' . $k . '>' . $v . '</option>';
+                                                    }
+                                                    ?>
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-1 drug-field">Year</div>
+                                        <div class="col-md-2">
+                                            <div class=" input-group">
+                                                <span class="input-group-addon" id="basic-addon1">
+                                                    <i class="fa fa-calendar"></i>
+                                                </span>
+                                                <select id="monthly-year" class="form-control">
+                                                    <option value="select">select</option>
+                                                    <?php
+                                                    $year = 2005;
+                                                    for ($year; $year <= 2030; $year++) {
+                                                        echo '<option value=' . $year . '>' . $year . '</option>';
+                                                    }
+                                                    ?>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-1 head">
+                                            <button type="button" class="btn btn-block btn-success" name="btn-monthly-drug" id="btn-monthly-drug">
+                                                <span class="fa fa-server"></span> Go</button>       
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="between" class="tab-pane fade">
+                                    <div class="tab-rep">
+                                        <div class="col-md-1 drug-field">Dates</div>
+                                        <div class="col-md-3">
+                                            <div class=" input-group">
+                                                <span class="input-group-addon" id="basic-addon1">
+                                                    <i class="fa fa-calendar"></i>
+                                                </span>
+                                                <input class="form-control" readonly type="text" id="start-date" name='start-date'/></input>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <div class=" input-group">
+                                                <span class="input-group-addon" id="basic-addon1">
+                                                    <i class="fa fa-calendar"></i>
+                                                </span>
+                                                <input class="form-control" type="text" readonly id="end-date" name='end-date'/></input>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-1 head">
+                                            <button type="button" class="btn btn-block btn-success" name="btn-between-drug" id="btn-between-drug">
+                                                <span class="fa fa-server"></span> Go</button>       
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="Yearly" class="tab-pane fade">
+                                    <div class="tab-rep">
+                                        <div class="col-md-1 drug-field">Select Year</div>
+                                        <div class="col-md-2">
+                                            <div class=" input-group">
+                                                <span class="input-group-addon" id="basic-addon1">
+                                                    <i class="fa fa-calendar"></i>
+                                                </span>
+                                                <select id="yearly-year" class="form-control">
+                                                    <option value="select">select</option>
+                                                    <?php
+                                                    $year = 2005;
+                                                    for ($year; $year <= 2030; $year++) {
+                                                        echo '<option value=' . $year . '>' . $year . '</option>';
+                                                    }
+                                                    ?>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-1 head">
+                                            <button type="button" class="btn btn-block btn-success" name="btn-yearly-drug" id="btn-yearly-drug">
+                                                <span class="fa fa-server"></span> Go</button>       
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- pdf viewer-->
+                <div class="pdf-viewer" id="pdfloader">
+                    <iframe id="frame" style="width: 100%; min-height: 500px; height: 100%;"></iframe>
+                </div> 
+                <!--end of pdf viewer-->
+            </div>
+            <!-- /#page-content-wrapper -->
+        </div>
+        <!-- /#wrapper -->
+        <script src="js/profile.js"></script>
+
+        <!--body div-->
+
+    </body>
+</html>
+
